5e3c373e7b6a52ac481bd5f5fa0172bba4cb30ce
Scott Davis <scottdavisxx@gmail.com>
2022-06-08 22:20:51 -0600

    create data for navigation list as array of objects

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -15,7 +15,25 @@ import {
   MdLibraryMusic,
   MdPlaylistAdd,
   MdFavorite
-} from "react-icons/md"
+} from "react-icons/md";
+
+const navMenu = [
+  {
+    name: "Home",
+    icon: MdHome,
+    route: "/",
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "/search",
+  },
+  {
+    name: "Your Library",
+    icon: MdLibraryMusic,
+    route: "/library",
+  },
+];
 
 const Sidebar = () => {
   return(
